480b3ba4dbdc739c9e6102edc554af94a6025aea
Joyce Villarroel Mascarenhas <joycevillarroel@MacBook-Air-de-Joyce.local>
2020-03-04 00:34:22 -0300

    recent small adjustments

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import Home from './components/home';
-import { Switch, Route } from 'react-router-dom';
 
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-
-
-          <Route exact path='/' component={Home}/>
- 
-       
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+     <h1>Colégio Fidens</h1>
+    </div>
+  );
 }
 
 export default App;
