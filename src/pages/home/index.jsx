4c8644be1ed2c0d1173fb42a6062445657b6abab
Joyce Villarroel Mascarenhas <joycevillarroel@MacBook-Air-de-Joyce.local>
2020-11-30 14:54:26 -0300

    about implementation

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Navbar from '../../components/navbar/navbar';
 import Footer from '../../components/footer/footer';
 import Banner from '../../components/banner/index';
+import About from '../../components/about';
 
 const Home = () => (
 <div>
     <Navbar/>
     <Banner/>
-
+    <About/>
     <Footer/>
 </div>
 )
